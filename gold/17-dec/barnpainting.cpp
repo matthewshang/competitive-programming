a878bac78ebf0281e9927c869b84609e3cae78f3
Matthew Shang <mshang47@gmail.com>
2019-02-06 13:10:21 -0800

    sync

--- gold/17-dec/barnpainting.cpp
+++ gold/17-dec/barnpainting.cpp
@@ -26,12 +26,14 @@ template <typename T> void pr(T *arr, int n) {
 const int MAXN = 10000;
 const ll MOD = 1000000007;
 
-set<int> graph[MAXN];
-int parent[MAXN];
-int dp[MAXN][3];
-
 int main() {
+    set<int> graph[MAXN];
+    int parent[MAXN];
+    ll dp[MAXN][3];
+    int mark[MAXN];
+    bool fixed[MAXN];
     int n, k;
+
     ifstream in("barnpainting.in");
     in >> n >> k;
     forn (i, n) parent[i] = -1;
@@ -53,13 +55,79 @@ int main() {
             graph[y].insert(x);
         }
     }
-    // forn (i, n) color[i] = 0;
-    // forn (i, k) {
-    //     int b, c;  in >> b >> c;
-    //     color[b - 1] = c;
-    // }
+
+    forn (i, n) {
+        forn (j, 3) dp[i][j] = 0;
+        fixed[i] = false;
+    }
+    forn (i, k) {
+        int b, c;  in >> b >> c;
+        dp[b - 1][c - 1] = 1;
+        fixed[b - 1] = true;
+    }
+    forn (i, n) {
+        cout << "node " << i << ": parent: " << parent[i] << endl;
+        for (int x : graph[i]) {
+            cout << x << " ";
+        }
+        cout << endl;
+        cout << "dp: " << dp[i][0] << ", " << dp[i][1] << ", " << dp[i][2] << endl;
+    }
+
+    queue<int> q;
+    ll answer = 1;
+    int root = -1;
+    forn (i, n) {
+        mark[i] = graph[i].size();
+        if (mark[i] == 0) {
+            q.push(i);
+            cout << "leaf: " << i << endl;
+        }
+        if (parent[i] == -1) root = i;
+    }
+    cout << "root: " << root << endl;
+
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+
+        if (mark[u] == -1) {
+            continue;
+        } else if (mark[u] != 0) {
+            q.push(u);
+            continue;
+        }
+
+        for (int x : graph[u]) {
+            dp[u][0] = dp[u][0] * (dp[x][1] + dp[x][2]);
+            dp[u][1] = dp[u][1] * (dp[x][0] + dp[x][2]);
+            dp[u][2] = dp[u][2] * (dp[x][0] + dp[x][1]);
+        }
+        if (fixed[u]) {
+            answer *= dp[u][0] + dp[u][1] + dp[u][2];
+        }
+        if (graph[u].size() == 0) {
+            forn (i, 3) dp[u][i] = 1;
+        }
+        
+        if (parent[u] != -1) {
+            mark[parent[u]]--;
+            q.push(parent[u]);
+        }
+    }
+
+    if (!fixed[root]) {
+        for (int x : graph[root]) {
+            dp[root][0] = dp[root][0] * (dp[x][1] + dp[x][2]);
+            dp[root][1] = dp[root][1] * (dp[x][0] + dp[x][2]);
+            dp[root][2] = dp[root][2] * (dp[x][0] + dp[x][1]);
+        }
+        answer *= dp[root][0] + dp[root][1] + dp[root][2];
+    }
+
 
         
+    cout << answer << endl;
     ofstream out("barnpainting.out");
     return 0;
 }
